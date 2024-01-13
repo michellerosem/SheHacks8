5d2f0d7d31da23be8844fe24c67dcac734c56c3a
Michelle <mmenor@uwo.ca>
2024-01-13 02:19:02 -0500

    Added Emergency Contact Title

--- AccountInfo.js
+++ AccountInfo.js
@@ -1,3 +1,18 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, Button } from 'react-native';
 
+const AccountInfo = () => {
+    const [emergencyContacts, setEmergencyContacts] = useState([]);
+    const [homeAddress, setHomeAddress] = useState('');
+
+    const addEmergencyContact = (contact) => {
+        setEmergencyContacts([...emergencyContacts, contact]);
+      };
+
+    return (
+        <View>
+            <Text>Emergency Contacts:</Text>
+        </View>
+    );
+};
+export default AccountInfo;
