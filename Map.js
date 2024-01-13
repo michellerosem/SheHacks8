baebca5285a1f599c26ea88dd809b4eb32652ebd
michellerosem <151082303+michellerosem@users.noreply.github.com>
2024-01-13 19:00:39 +0000

    Added map file

--- Map.js
+++ Map.js
@@ -2,13 +2,13 @@ import React from 'react';
 import MapView from 'react-native-maps';
 import { StyleSheet, View } from 'react-native';
 
-export default function MapView() {
-  return (
-    <View style={styles.container}>
-      <MapView style={styles.map} />
-    </View>
-  );
-}
+// export default function MapView() {
+//   return (
+//     <View style={styles.container}>
+//       <MapView style={styles.map} />
+//     </View>
+//   );
+// }
 
 const styles = StyleSheet.create({
   container: {
