a17f1914808243c96ad5861a5e617a9f1949ed6c
michellerosem <151082303+michellerosem@users.noreply.github.com>
2024-01-14 01:44:20 +0000

    added text

--- StudySpot.js
+++ StudySpot.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import { View, TextInput, Button, Text, StyleSheet } from 'react-native';
 
-const AccountInfo = () => {
+const StudySpots = () => {
 
     return(
         <ScrollView contentContainerStyle={styles.container}>
+            <Text style={styles.header}> Study Spots </Text>
 
-            
         </ScrollView>
     )
 }
+export default StudySpots;
